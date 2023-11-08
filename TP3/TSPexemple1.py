a841c45a2d7f9e4458c4882a604194e152187794
thibault-chausson <chausson.thibault@laposte.net>
2023-11-08 13:01:57 +0100

    :construction: tp3

--- TP3/TSPexemple1.py
+++ TP3/TSPexemple1.py
@@ -25,7 +25,7 @@ def generer_ville(nombre_de_ville, max_distance):
         for vers_la_ville in villes:
             distances[ville][vers_la_ville] = random.randint(50, max_distance)
             distances[vers_la_ville][ville] = distances[ville][vers_la_ville]
-    print('voici la matrice des distances entres les villes \n', distances)
+    # print('voici la matrice des distances entres les villes \n', distances)
     return distances
 
 
@@ -44,17 +44,16 @@ def voisinage(solution, nombre_de_ville):
     return sol_voisine
 
 
-def recuit(nombre_de_ville, max_distance, T, facteur, tour):
+def recuit_simule(nombre_de_ville, distances, T, facteur, tour):
     # recuit simulé
-    distances = generer_ville(nombre_de_ville, max_distance)
+    # distances = generer_ville(nombre_de_ville, max_distance)
     solution = random.sample(range(nombre_de_ville), nombre_de_ville)
     cout0 = cal_distance(solution, distances, nombre_de_ville)
     # T_intiale = max_distance / 2
     min_sol = solution
     cout_min_sol = cout0
     for i in range(tour):
-        print('la ', i, 'ème solution = ', solution, ' donne la distance totale= ', cout0, ' la température actuelle =',
-              T)
+        # print('la ', i, 'ème solution = ', solution, ' donne la distance totale= ', cout0, ' la température actuelle =', T)
         T = T * facteur
         for j in range(50):
             nouv_sol = voisinage(solution * 1, nombre_de_ville)
@@ -72,7 +71,9 @@ def recuit(nombre_de_ville, max_distance, T, facteur, tour):
                     cout0 = cout1
                     solution = nouv_sol
 
-    print('voici la solution retenue ', min_sol, ' et son coût ', cal_distance(min_sol, distances, nombre_de_ville))
+    distance_totale = cal_distance(min_sol, distances, nombre_de_ville)
+    # print('voici la solution retenue ', min_sol, ' et son coût ', distance_totale)
+    return min_sol, distance_totale
 
 
-recuit(NOMBRE_DE_VILLES, MAX_DISTANCE, TEMPERATURE, FACTEUR, TOUR)
+print(recuit_simule(NOMBRE_DE_VILLES, generer_ville(NOMBRE_DE_VILLES, MAX_DISTANCE), TEMPERATURE, FACTEUR, TOUR))
